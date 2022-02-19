fa05b4e1d56eb247485c04fc61bf19b0e5d3a9a5
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-02-19 02:53:34 -0600

    react app

---
+++ js/app-1.js
@@ -0,0 +1,9 @@
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                Hello, friend! I'm a React Component.
+            </div>
+        );
+    }
+}
